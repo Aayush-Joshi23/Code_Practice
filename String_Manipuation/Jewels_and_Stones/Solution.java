0a5c5b023008cad7b9c32c768fb24977b49bc901
Aayush Joshi <69706553+Aayush-Joshi23@users.noreply.github.com>
2022-09-14 19:21:05 +0530

    Add files via upload

---
+++ String_Manipuation/Jewels_and_Stones/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        
+        // int n = jewels.length(), m = stones.length();
+
+        // HashSet<Character> hset = new HashSet<>();
+        int c = 0;
+        
+//         for (int i = 0; i<n; i++)
+//         {
+//             hset.add(jewels.charAt(i));
+//         }
+        
+//         for(int i = 0; i<m ; i++)
+//         {
+//             if(hset.contains(stones.charAt(i)))
+//             {
+//                 c++; 
+//             }
+//         }
+        
+        for(char ch:stones.toCharArray())
+        {
+            if(jewels.indexOf(ch) != -1)
+            {
+                c++; 
+            }
+        }
+        return c;
+        
+        
+    }
+}
