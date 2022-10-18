b7f794a95c264fb8d0cbbaf23df872165df5d42b
Aayush Joshi <69706553+Aayush-Joshi23@users.noreply.github.com>
2022-10-18 12:46:44 +0530

    Add files via upload

---
+++ Linked_List/6--Delete_Node_in_a_Linked_List/Solution_0ms.java
@@ -0,0 +1,24 @@
+import JAVA.Linked_List.ListNode;
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+
+ 
+class Solution {
+    public void deleteNode(ListNode node) {
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+
+    }
+}
