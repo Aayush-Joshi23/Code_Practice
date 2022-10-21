35b0e180e94b31974338ef51c821f3a09a87a906
Aayush Joshi <69706553+Aayush-Joshi23@users.noreply.github.com>
2022-10-21 17:57:38 +0530

    Add files via upload

---
+++ Linked_List/8--Intersection_of_Two_Linked_List/Solution_OptimizedShortCode.java
@@ -0,0 +1,27 @@
+import JAVA.Linked_List.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution_OptimizedShortCode {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+        return a;
+
+    }
+}
