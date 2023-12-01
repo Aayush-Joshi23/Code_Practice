36a40cfcde6648dc983974724ef03a1954a74289
Aayush Joshi <69706553+Aayush-Joshi23@users.noreply.github.com>
2023-12-01 21:41:58 +0530

    Add files via upload

---
+++ String_Manipuation/14--Check_If_Two_String_Arrays_are_Equivalent/Solution_2loops.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        
+        String st1 ="", st2="";
+        
+        for(String st: word1)
+        {
+            st1 += st;
+        }
+        for(String st: word2)
+        {
+            st2 += st;
+        }
+        return (st1.equals(st2));
+    }
+}
