53a559e8bf75896da7a30689c12b1cad4a6c97d6
Aayush Joshi <69706553+Aayush-Joshi23@users.noreply.github.com>
2024-01-17 16:19:22 +0530

    Add files via upload

---
+++ Dynamic_Programming/1--Jump_Game/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        
+        int pos = 0;
+
+        for(int i =0; i<nums.length; i++)
+        {
+            if(i>pos) 
+                return false;
+            pos = Math.max(pos, i+nums[i]);
+            System.out.println(pos+"  "+i+"  "+nums[i]);
+        }
+        return true;
+        
+    }
+}
