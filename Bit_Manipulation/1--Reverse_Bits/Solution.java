ea9c7b3633ad8da4f6b265cd26f705c551e4f079
Aayush Joshi <69706553+Aayush-Joshi23@users.noreply.github.com>
2024-02-18 15:19:39 +0530

    Add files via upload

---
+++ Bit_Manipulation/1--Reverse_Bits/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+
+        int result=0,i;
+        for(i=0;i<32;i++)
+        {
+            result = result<<1;
+            result = result|(n&1);
+            n = n>>1;
+        }
+        return result;
+        
+    }
+}
